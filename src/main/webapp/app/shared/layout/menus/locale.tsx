b27745eb358fe61d8c3ccf7b0811e364e02a13a4
Mark Tripoli <mgtripoli@comcast.net>
2021-07-19 23:07:42 -0400

    Menu Fixes

--- src/main/webapp/app/shared/layout/menus/locale.tsx
+++ src/main/webapp/app/shared/layout/menus/locale.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { locales, languages } from 'app/config/translation';
 import { Dropdown, DropdownMenu, DropdownToggle, DropdownItem } from 'mdbreact';
+import { findFlag } from 'app/shared/util/flag-utils';
 
 export const LocaleMenu = ({ currentLocale, onClick }: { currentLocale: string; onClick: (event: any) => void }) =>
   Object.keys(languages).length > 1 ? (
     <Dropdown className="d-flex">
       <DropdownToggle tag="language" className="nav-link text-reset dropdown-toggle me-3 hidden-arrow">
-        <i className={currentLocale ? languages[currentLocale].flag : 'america flag m-0'}></i>
+        {`${currentLocale.toUpperCase()} ${findFlag(currentLocale)}`}
       </DropdownToggle>
       <DropdownMenu>
         {locales.map(locale => (
           <DropdownItem key={locale} value={locale} onClick={onClick}>
-            <i className={languages[locale].flag}></i>
+            {`${locale.toUpperCase()} ${findFlag(locale)}`}
           </DropdownItem>
         ))}
       </DropdownMenu>
